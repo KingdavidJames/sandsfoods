2a33a0c38d8e9d93d92ba681595d82d22f57a29d
KingdavidJames <71078691+KingdavidJames@users.noreply.github.com>
2020-11-02 15:13:41 +0100

    Add files via upload

---
+++ sandsfoods/js/main.js
@@ -0,0 +1,16 @@
+
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $("#preloader, .pre, .img1").delay(2).fadeOut("slow");
+    });
+
+  
+
+})(jQuery);
